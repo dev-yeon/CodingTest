0e776c8b1885324fd6f6411ebca352dffe4d5ae1
dev-yeon <nochi@kakao.com>
2024-09-11 01:01:08 +0900

    MegaFE 폴더 추가
    -html
    -Js
    
    폴더가 있는데 js폴더에 자바 스크립트 최근에 수업 나간거 까지의 작업물이 저장되어 있습니다.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("hello, world");
+
+console.log()
+
+function sum100() {
+  let sum=0;
+  for(let i =0; i<=100; i++){
+    sum += i;
+    
+  }
+  return sum
+}
+
+const k = sum100();
+const x = sum100();
+const y = sum100();
+console.log(k);
+

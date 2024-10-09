8abf43e44b8555e5e1f56a6605a2f97d0002081f
dev-yeon <nochi@kakao.com>
2024-10-09 23:49:57 +0900

    24.10.09
    - Python

---
+++ Python/Nomad/2.6 Parameters.py
@@ -0,0 +1,12 @@
+
+def say_hello(user_name):
+  print("hello", user_name,'how r u?')
+
+# user_name 은 parameter 이라고 하고,
+# nico , lynn , lewis , ralph 등의 실제로 전달한 데이터는 argument 라고 한다.
+say_hello("nico")
+say_hello("lynn")
+say_hello("lewis")
+say_hello("ralph")
+
+# 둘다 function 이 받는 값을 나타낸다. 
